26c788f98d5c597f96b6481378a6c2a2f63170db
Bill Wang <101487342+billatgameology@users.noreply.github.com>
2024-03-31 07:37:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
-
-port = int(os.environ.get('PORT', 8080))
-app.run(host='0.0.0.0', port=port)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))  # Default port or one provided by Cloud Run environment
+    app.run(debug=True, host='0.0.0.0', port=port)
