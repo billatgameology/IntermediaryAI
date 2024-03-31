f4387b1d473009f3e82c28935cbd73224d8dc97e
billatgameology <101487342+billatgameology@users.noreply.github.com>
2024-03-31 15:44:41 -0700

    removed some code

--- app.py
+++ app.py
@@ -4,47 +4,47 @@ from flask import Flask
 
 app = Flask(__name__)
 
-genai.configure(api_key=os.environ.get("Gemini-API-Key"))
+# genai.configure(api_key=os.environ.get("Gemini_API_Key"))
 
-# Set up the model
-generation_config = {
-  "temperature": 0.9,
-  "top_p": 1,
-  "top_k": 1,
-  "max_output_tokens": 2048,
-}
+# # Set up the model
+# generation_config = {
+#   "temperature": 0.9,
+#   "top_p": 1,
+#   "top_k": 1,
+#   "max_output_tokens": 2048,
+# }
 
-safety_settings = [
-  {
-    "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-]
+# safety_settings = [
+#   {
+#     "category": "HARM_CATEGORY_HARASSMENT",
+#     "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+#   },
+#   {
+#     "category": "HARM_CATEGORY_HATE_SPEECH",
+#     "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+#   },
+#   {
+#     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+#     "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+#   },
+#   {
+#     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+#     "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+#   },
+# ]
 
-model = genai.GenerativeModel(model_name="gemini-1.0-pro",
-                              generation_config=generation_config,
-                              safety_settings=safety_settings)
+# model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+#                               generation_config=generation_config,
+#                               safety_settings=safety_settings)
 
-convo = model.start_chat(history=[
-])
+# convo = model.start_chat(history=[
+# ])
 
-convo.send_message("hi")
+# convo.send_message("hi")
 
 @app.route('/')
 def hello_world():
-    return convo.last.text
+    return "Hello world" #convo.last.text
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))  # Default port or one provided by Cloud Run environment
