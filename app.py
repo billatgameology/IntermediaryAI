fb87cdaeb3ba378244a348532e60b1e3eb505cc8
billatgameology <101487342+billatgameology@users.noreply.github.com>
2024-03-31 15:11:34 -0700

    updated return message

--- app.py
+++ app.py
@@ -1,47 +1,50 @@
 import os
+import google.generativeai as genai
 from flask import Flask
 
 app = Flask(__name__)
 
+genai.configure(api_key=os.environ.get("Gemini-API-Key"))
 
-
-
-import base64
-import vertexai
-from vertexai.generative_models import GenerativeModel, Part
-import vertexai.preview.generative_models as generative_models
-
-
-def multiturn_generate_content():
-  vertexai.init(project="project-ada-418422", location="us-central1")
-  model = GenerativeModel(
-    "gemini-1.0-pro-001",
-  )
-  chat = model.start_chat()
-  print(chat.send_message(
-      ["""hello"""],
-      generation_config=generation_config,
-      safety_settings=safety_settings
-  ))
-
-
+# Set up the model
 generation_config = {
-    "max_output_tokens": 2048,
-    "temperature": 0.9,
-    "top_p": 1,
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
 }
 
-safety_settings = {
-    generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-}
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
 
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat(history=[
+])
+
+convo.send_message("hi")
 
 @app.route('/')
 def hello_world():
-    return multiturn_generate_content()
+    return convo.last.text
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))  # Default port or one provided by Cloud Run environment
