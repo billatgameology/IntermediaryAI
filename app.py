77ddbd335e7f1af4504afe1047ca50b3d4ed0536
Bill Wang <101487342+billatgameology@users.noreply.github.com>
2024-03-31 07:30:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
+port = int(os.environ.get('PORT', 8080))
+app.run(host='0.0.0.0', port=port)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
